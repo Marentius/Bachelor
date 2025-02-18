7667a92add63d9396a40369439180e74b406ee62
Marentius <vetlenilseen@hotmail.com>
2025-02-18 23:07:18 +0100

    Update WebSocket configuration and remove unnecessary dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import { MapContainer, TileLayer} from 'react-leaflet'
 import './App.css'
+// Importerer og starter WebSocket-klienten som kobler til backend
+import './service/WebSocket'
 
 function App() {
   return (
     <div className="app-container">
+      {/* MapContainer er hovedkomponenten for kartet fra react-leaflet */}
       <MapContainer 
+        // Setter startposisjon for kartet til midt i Norge [breddegrad, lengdegrad]
         center={[65, 13]} 
+        // Setter startnivå for zoom
         zoom={5} 
+        // Tillater zooming med musehjul
         scrollWheelZoom={true}
+        // Setter kartet til å fylle hele nettleservinduet
         style={{ height: "100vh", width: "100vw" }}
       >
+        {/* TileLayer laster inn selve kartbildene fra OpenStreetMap */}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -20,3 +28,8 @@ function App() {
 }
 
 export default App
+
+// Kilder
+// https://react-leaflet.js.org/docs/start-installation/
+// https://react-leaflet.js.org/docs/start-setup/
+// https://leafletjs.com/examples/quick-start/
