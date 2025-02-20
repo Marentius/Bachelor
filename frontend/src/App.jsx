75433b804f7be1f08ba46ffb1cdb29f1234ffbd9
Marentius <vetlenilseen@hotmail.com>
2025-02-20 22:48:55 +0100

    changed structure for components and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,9 @@
-import { MapContainer, TileLayer} from 'react-leaflet'
+import Map from './components/Map';
 import './App.css'
 
 function App() {
   return (
-    <div className="app-container">
-      <MapContainer 
-        center={[65, 13]} 
-        zoom={5} 
-        scrollWheelZoom={true}
-        style={{ height: "100vh", width: "100vw" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
-    </div>
+    <Map />
   );
 }
 
