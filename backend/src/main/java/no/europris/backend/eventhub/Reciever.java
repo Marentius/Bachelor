22b09ddc7ea50517d0af3ab1c35787257b335a0f
Enerhaugen <robin.enerhaugen@hotmail.com>
2025-02-26 09:42:34 +0100

    Lagt til fasterxml i pom.xml, lagt til kategorisering basert på verdien på kvitteringen

--- backend/src/main/java/no/europris/backend/eventhub/Reciever.java
+++ backend/src/main/java/no/europris/backend/eventhub/Reciever.java
@@ -1,13 +1,15 @@
 package no.europris.backend.eventhub;
 
 import com.azure.messaging.eventhubs.*;
-
 import com.azure.messaging.eventhubs.models.EventPosition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import jakarta.annotation.PostConstruct;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode; // Importer JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 // Denne klassen er ansvarlig for å motta events fra Azure Event Hub og videresende dem til WebSocket
 @Component // Gjør at Spring Boot automatisk oppretter en instans av denne klassen
 public class Reciever {
@@ -71,14 +73,48 @@ public class Reciever {
         }
     }
 
+    
     // Håndterer hvert enkelt event som mottas
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    
     private void processEvent(EventData event) {
-        // Konverterer event-data til string
-        String eventData = event.getBodyAsString();
+        try{
+        //For å unngå og opprette Java-objekter bruker vi JsonNode som lar oss jobbe dynamisk med Json-data.
+
+        //Oppretter ett JsonNode objekt og konverterer event-data til string
+
+        JsonNode jsonNode = objectMapper.readTree(event.getBodyAsString());
+
+        //Henter ut feltet vi skal bruke for å kategorisere dataen som double
+
+        Double receiptTotalIncVat = jsonNode.path("receiptTotalIncVat").asDouble();
+      
+        //Legger til kategori basert på verdien i feltet, verdiene skal endres i etterkant
+
+        if (jsonNode.get("receiptTotalIncVat").asDouble() >= 300 && jsonNode.get("receiptTotalIncVat").asDouble() < 800) {
+            ((ObjectNode) jsonNode).put("saleSizeCategory", 2);
+        }
+
+        else if (receiptTotalIncVat >= 800){
+            ((ObjectNode) jsonNode).put("saleSizeCategory", 3);
+        }
+        
+        else {
+            ((ObjectNode) jsonNode).put("saleSizeCategory", 1);
+        }
+
         // Logger til konsoll
-        System.out.printf("Mottok event med innhold: %s%n", eventData);
+        System.out.printf("Mottok event med innhold: %s%n", jsonNode.toString());
+
         // Sender eventet til alle tilkoblede WebSocket-klienter på topic "receipts"
-        messagingTemplate.convertAndSend("/topic/receipts", eventData);
+        messagingTemplate.convertAndSend("/topic/receipts", jsonNode.toString());
+        }
+        catch(JsonProcessingException e){
+            e.printStackTrace();
+        }
+        
+        
     }
 
     // Enkel feilhåndtering - logger feilmeldingen
@@ -90,4 +126,6 @@ public class Reciever {
  * Kilder:
  * https://learn.microsoft.com/en-us/azure/event-hubs/event-hubs-java-get-started-send?tabs=passwordless%2Croles-azure-portal
  * https://learn.microsoft.com/en-us/azure/developer/java/spring-framework/configure-spring-cloud-stream-binder-java-app-azure-event-hub?toc=%2Fazure%2Fevent-hubs%2FTOC.json
+ * https://medium.com/@salvipriya97/jsonnode-explained-with-examples-d0c05324f61d
+ * https://www.baeldung.com/jackson-json-node-tree-model?utm_source=chatgpt.com
  */
