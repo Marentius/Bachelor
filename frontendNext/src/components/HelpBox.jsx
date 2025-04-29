a66d82fa0511da9b0e841724f27f7d5a137f7ba9
Marentius <vetlenilseen@hotmail.com>
2025-04-29 11:53:51 +0200

    version B launch

--- frontendNext/src/components/HelpBox.jsx
+++ frontendNext/src/components/HelpBox.jsx
@@ -10,18 +10,19 @@ export default function HelpBox() {
                 </li>
                 <li className="list-item">
                     <img src="standardFlower.svg" alt="Blomst" className="list-img"/>
-                    <p>Lite salg: 0kr - 300kr</p>
+                    <p>Viser salgsstørrelse</p>
                 </li>
                 <li className="list-item">
-                    <img src="mediumSustainable.svg" alt="Blomst" className="list-img"/>
-                    <p>Medium salg: 300kr - 1000kr</p>
+                    <p><strong>Liten blomst (30px):</strong> Lite salg: 0kr - 300kr</p>
                 </li>
                 <li className="list-item">
-                    <img src="sustainableFlower.svg" alt="Blomst" className="list-img"/>
-                    <p>Stort salg: over 1000kr</p>
+                    <p><strong>Medium blomst (60px):</strong> Medium salg: 300kr - 1000kr</p>
                 </li>
                 <li className="list-item">
-                    <img src="warehouseRedDot.png" alt="Blomst" className="list-img"/>
+                    <p><strong>Stor blomst (120px):</strong> Stort salg: over 1000kr</p>
+                </li>
+                <li className="list-item">
+                    <img src="warehouseRedDot.png" alt="Europris varehus" className="list-img"/>
                     <p>Europris varehus</p>
                 </li>
             </ul>
--- frontendNext/src/icons/Icons.js
+++ frontendNext/src/icons/Icons.js
@@ -10,22 +10,18 @@ const createFlowerIcon = (saleSizeCategory) => {
     // Bestem størrelsen basert på kategori
     // Kategori 1 = liten, 2 = medium, 3 = stor
     let size;
-    let flower;
     if (saleSizeCategory === 1) {
         size = 30; // Liten blomst
-        flower = 'standardFlower.svg'
     } else if (saleSizeCategory === 2) {
         size = 60; // Medium blomst
-        flower = 'mediumSustainable.svg'
     } else if (saleSizeCategory === 3) {
         size = 120; // Stor blomst
-        flower = 'sustainableFlower.svg'
     } else {
         size = 30; // Standard størrelse hvis kategori er ukjent
     }
     
     return L.divIcon({
-        html: `<img src="/${flower}" class="animated-flower" alt="Blomst" style="width:${size}px; height:${size}px;" />`,
+        html: `<img src="/standardFlower.svg" class="animated-flower" alt="Blomst" style="width:${size}px; height:${size}px;" />`,
         className: 'transparent-flower',
         iconSize: [size, size],
         iconAnchor: [size/2, size]
